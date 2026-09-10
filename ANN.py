0ecdad63d49fdfb212abeaa0ab9119ff99a47f2b
srimuthukrishnan391 <srimuthukrishnan391@gmail.com>
2026-09-10 09:10:20 +0530

    A Multi-Layer Perceptron contains multiple fully connected layers.
    Where used: Classification, regression and general prediction.
    
    Why needed: It can learn non-linear relationships between input and output

---
+++ ANN.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+
+    def __init__(self, input_size):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.network(x)
